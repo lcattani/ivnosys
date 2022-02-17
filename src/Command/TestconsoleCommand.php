a84ccf69644e4e91498df14fd9d915fae8d1b394
Leonardo Cattani <Nop78109>
2022-02-17 22:12:59 +0000

    Finalizada la etapa 2 Comenzados los cambios para aceptar consola

---
+++ src/Command/TestconsoleCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Command;
+
+use App\Controller\LawsuitsController;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class TestconsoleCommand extends Command
+{
+    protected static $defaultName = 'lawsuits';
+    protected static $defaultDescription = 'We are in the era of "lawsuits", everyone wants to go to court with their lawyer Saul and try to get a lot of dollars as if they were raining over Manhattan.';
+
+    protected function configure(): void
+    {
+        $this
+        ->addArgument('Plaintiff signatures', InputArgument::REQUIRED, 'The Plaintiff signatures.')
+        ->addArgument('Defendant signatures', InputArgument::REQUIRED, 'The Defendant signatures.')
+        //->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $plaintiff = $input->getArgument('Plaintiff signatures');
+        $defendant = $input->getArgument('Defendant signatures');
+
+        // Parametros pasados
+        $io->note("You passed\n Plaintiff signatures: {$plaintiff}\n Defendant signatures: {$defendant}");
+
+        /*
+        if ($input->getOption('option1')) {
+            // ...
+        }*/
+
+     
+
+
+        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+
+        return Command::SUCCESS;
+    }
+}
--- src/Controller/LawsuitsController.php
+++ src/Controller/LawsuitsController.php
@@ -9,22 +9,41 @@ use Symfony\Component\HttpFoundation\Request;
 
 class LawsuitsController extends AbstractController
 {
+
+    private $plaintiff; 
+    private $defendant;
+
+
     /**
      * @Route("/lawsuits", name="lawsuits")
      */
     public function index(Request $request): Response
     {
-        $plaintiff = $request->query->get('plaintiff');
-        $defendant = $request->query->get('defendant');
 
-        $plaintiff = $this->parseSignatures($plaintiff);
-        $defendant = $this->parseSignatures($defendant);
+        $this->plaintiff = $request->query->get('plaintiff');
+        $this->defendant = $request->query->get('defendant');
 
-        $winner = $plaintiff["points"] > $defendant["points"] ? "plaintiff" : "defendant";
+        $this->process();
+
         return $this->render('lawsuits/index.html.twig', [
             'controller_name' => 'LawsuitsController',
-            'result' => ['plaintiff' => $plaintiff, 'defendant' => $defendant, "winner" => $winner],
+            'result' => ['plaintiff' => $this->plaintiff, 'defendant' => $this->defendant],
         ]);
+
+
+    }
+
+    private function process()
+    {
+
+        $this->plaintiff = $this->parseSignatures($this->plaintiff);
+        $this->defendant = $this->parseSignatures($this->defendant);
+
+        $this->setWinner($this->plaintiff, $this->defendant);
+
+        $this->plaintiff = $this->analyzeJoker($this->plaintiff, $this->defendant["points"] - $this->plaintiff["points"]);
+        $this->defendant = $this->analyzeJoker($this->defendant, $this->plaintiff["points"] - $this->defendant["points"]);
+
     }
 
     private function getRols(): array
@@ -52,24 +71,97 @@ class LawsuitsController extends AbstractController
 
         $signatures_array = str_split($signatures);
         $rols = $this->getRols();
+        $jokers = 0;
+
+        $king = strpos($signatures, "K") !== false ? true : false;
+        $joker = strpos($signatures, "#") !== false ? true : false;
+
         $result["signatures"]  = $signatures;
         $result["error"]  = 0;
         $result["points"] = 0;
-
-        $king = strpos($signatures, "K");
-        $joker = strpos($signatures, "#");
+        $result["joker"] = $joker;
+        $result["king"] = $king;
 
         foreach ($signatures_array as $signature) {
             if (!isset($rols[$signature])) {
                 $result["error"] = 1;
+                $result["points"] = 0;
                 break;
             } else {
-                if (!($signature == "V" && $king !== FALSE)) {
+                if($signature == "#"){
+                    $jokers++;
+                }                
+                else if (!($signature == "V" && $king == TRUE)) {
                     $result["points"] += $rols[$signature]["points"];
                 }
             }
         }
 
+        // Solo una Joker es permitido
+        if ($jokers > 1) {
+            $result["error"] = 1;
+            $result["points"] = 0;
+        }
+
         return $result;
+    }
+
+    private function setWinner()
+    {
+
+        $this->plaintiff['winner'] = $this->plaintiff['points'] - $this->defendant['points'];
+        $this->defendant['winner'] = $this->defendant['points'] - $this->plaintiff['points'];
+        
+    }
+
+    private function analyzeJoker(array $parsedSignatures, int $minimumToWin): array
+    {
+
+        // No existe Joker para analizar o ya es el ganador
+        if ($parsedSignatures['joker'] == false /*|| $minimumToWin < 0*/) {
+            $parsedSignatures['joker_analysis']['found'] = false;
+            return $parsedSignatures;
+        }
+
+        $to_win = array();
+
+        // Roles ordenados por puntos
+        $rols = $this->getRols();
+        uasort($rols, function ($a, $b) {
+            return $a['points'] - $b['points'];
+        });
+
+        // Ya es un ganador, analizo maximo alcanzable
+        if ($minimumToWin < 0) {
+            $key = array_key_last($rols);
+            $to_win = $rols[$key];
+        } else { // Analizo firma minima para ganar
+            $to_win = array();
+
+            // Al existir un King el Validator no cuenta
+            if ($parsedSignatures['king'])
+                unset($rols['V']);
+
+            foreach ($rols as $key => $rol) {
+                if ($rol['points'] > $minimumToWin) {
+                    $to_win = $rol;
+                    break;
+                }
+            }
+        }
+
+        // Valor de Joker encontrado
+        if (count($to_win) > 0) {
+            $to_win['new_signature'] = str_replace("#", $key, $parsedSignatures["signatures"]);
+            $to_win['found'] = true;
+            $new_signature = $this->parseSignatures($to_win['new_signature']);
+            $to_win['new_points'] = $new_signature['points'];
+
+            $parsedSignatures['joker_analysis'] = $to_win;
+        } else {
+            $parsedSignatures['joker_analysis']['found'] = false;
+        }
+
+        return $parsedSignatures;
     }
 }
